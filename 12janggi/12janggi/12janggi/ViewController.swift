ef7674899aaa1b256a639450cfe5d274beac3f7a
백승훈 <46189249+Walkers15@users.noreply.github.com>
2019-08-06 15:13:45 +0900

    Add files via upload

---
+++ 12janggi/12janggi/12janggi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  12janggi
+//
+//  Created by Release on 06/08/2019.
+//  Copyright © 2019 Hoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
